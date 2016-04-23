68ef2aa3f7ec5a9721ac54952f6f1484d4c82cf6
Ethan Urie <ethan.urie@gmail.com>
2016-04-23 14:08:32 -0400

    Initial Commit

---
+++ DNS Switcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DNS Switcher
+//
+//  Created by Ethan Urie on 4/23/16.
+//  Copyright © 2016 Strange Quark Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
